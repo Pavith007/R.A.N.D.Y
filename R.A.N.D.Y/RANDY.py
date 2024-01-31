acb80e0bcaf7589b2c88e9a4e1810f083002bc9c
Pavith Ashwin G.K <122193301+Pavith007@users.noreply.github.com>
2024-01-31 16:16:54 +0530

    Update RANDY1.py

--- R.A.N.D.Y/RANDY.py
+++ R.A.N.D.Y/RANDY.py
@@ -8,7 +8,7 @@ Engine.setProperty('voices',voices[0].id)
 def Speech(audio):
     Engine.say(audio)
     Engine.runAndWait()
-"""
+
 def Orders():
     r = sr.Recognizer()
     with sr.Microphone() as Source:
@@ -28,27 +28,3 @@ def Orders():
     return query
 query=Orders().lower()  
     
-    """
-def takeCommand():
-     
-    r = sr.Recognizer()
-     
-    with sr.Microphone() as source:
-         
-        print("Listening...")
-        r.pause_threshold = 1
-        r.adjust_for_ambient_noise(source,duration=1)
-        audio = r.listen(source)
-  
-    try:
-        print("Recognizing...")    
-        query = r.recognize_google(audio, language ='en-in')
-        print(f"User said: {query}\n")
-  
-    except Exception as e:
-        print(e)    
-        print("Unable to Recognize your voice.")  
-        return "None"
-     
-    return query
-takeCommand()

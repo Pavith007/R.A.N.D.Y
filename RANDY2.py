75722aefba04b3a8e73743ade82116271a1696a1
Pavith Ashwin G.K <122193301+Pavith007@users.noreply.github.com>
2024-02-13 12:27:07 +0530

    Update RANDY2.py
    Updated

--- RANDY2.py
+++ RANDY2.py
@@ -2,10 +2,29 @@ import pyttsx3
 import speech_recognition as sr
 import datetime
 import os
+import wikipedia
+import pywhatkit
+import pyautogui
+import pyjokes
+import webbrowser
+import openai
+
+openai.api_key= "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"
 
 Engine = pyttsx3.init('sapi5')
 voices= Engine.getProperty('voices')
 Engine.setProperty('voices',voices[0].id)
+
+def chat_gpt(prompt):
+    response = openai.Completions.create(
+    model="text-davinci-003"
+    prompt=prompt,
+    max_tokens=4000,
+    n=1,
+    stop=None,
+    temperature=0.5,
+    )
+    return response["choices"][0]["text"]
 
 def Speech(audio):
     Engine.say(audio)
@@ -14,13 +33,13 @@ def Speech(audio):
 def takeCommand():
      
     r = sr.Recognizer()
-     
     with sr.Microphone() as source:
          
         print("Listening...Boss")
         r.pause_threshold = 1
-        r.adjust_for_ambient_noise(source,duration=1)
+        r.adjust_for_ambient_noise(source,duration=0.5)
         audio = r.listen(source)
+        print("Wait a few minitues boss")
   
     try:
         print("Recognizing...")    
@@ -35,26 +54,88 @@ def takeCommand():
         return "None"
      
     return query
+
 def Greeting():
     Hour = int(datetime.datetime.now().hour)
     if Hour>=0 and Hour<=12:
-        print("Good Morning my King")
-        Speech("Good Morning my king")
-    elif Hour>12 and Hour<=4 :
-        print("Good Afternoon my king")
-        Speech("Good Afternoon my king")
-    elif Hour>4 and Hour<=7:
-        print("Good Evening my king")
-        Speech("Good Evening my king")
+        print("Good Morning sir")
+        Speech("Good Morning sir")
+    elif Hour>12 and Hour<=16 :
+        print("Good Afternoon sir")
+        Speech("Good Afternoon sir")
+    elif Hour>16 and Hour<=19:
+        print("Good Evening sir")
+        Speech("Good Evening sir")
     else:
-        print("Good Night my king")
-        Speech("Good Night my king")
-        
+        print("Good Night sir")
+        Speech("Good Night sir")
+def WakeUpCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+         
+        print("R.A.N.D.Y is Sleeping")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source,duration=1)
+        audio = r.listen(source)
+    try:
+        query = r.recognize_google(audio, language ='en-in')
+        print(f"User said: {query}\n")
+    except Exception as e:
+        query="none"
+    return query
+
+
+
 if __name__=="__main__":
-    Greeting()
-    query=takeCommand()
-    if 'time' in query:
-        Strtime = datetime.datetime.now().strftime("%H:%M:%S")
-        print(Strtime)
-        Speech(f"Sire,the time is {Strtime}")
-        
+    while True:
+        query=WakeUpCommand().lower()
+        if 'wake up' in query:
+            Greeting() 
+            Speech("Yes Sir ")
+            while True:
+                query=takeCommand().lower()
+                if 'time' in query:
+                    Strtime = datetime.datetime.now().strftime("%H:%M:%S")
+                    print(Strtime)
+                    Speech(f"Sire,the time is {Strtime}")
+                elif 'randy' in query:
+                    Speech("Yes boss")
+                    
+                elif 'open edge' in query:
+                    Speech("Opening the Edge...")
+                    os.startfile("C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe")
+                elif 'wikipedia' in query:
+                    Speech("Searching in wikipedia....")
+                    try:
+                        query=query.replace("wikipedia","")
+                        results = wikipedia.summary(query,sentences=2)
+                        Speech("Accoring to Wikipedia , ")
+                        print(results)
+                        Speech(results)
+                    except:
+                        Speech("No information found...")
+                        print("No information found...") 
+                elif 'play' in query:
+                    query=query.replace('play','')
+                    Speech('Playing the video' + query)
+                    pywhatkit.playonyt(query)
+                elif 'type' in query:
+                    Speech("plzz tell me What should i write boss")
+                    while True:
+                        InNotepad=takeCommand()
+                        if InNotepad=='exit':
+                            Speech("Done sir")
+                        else:
+                            pyautogui.write(InNotepad)
+                elif 'joke' in query:
+                    joke=pyjokes.get_joke()
+                    Speech(joke)
+                    print(joke)
+                elif 'Google' in query:
+                    Speech("Opeing Google Sir...")
+                    webbrowser.open("www.google.com")
+                elif 'sleep' in query:
+                    Speech("I am leaving sir, Bye!")
+                    quit()
+                    #make OpenAI /Whatapp Accesss / Google access ? Youtube / Hotkeys
+                    #bug in wikipedia
